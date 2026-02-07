b8b89fdd8cf4b04dd7f99be4820a0626b7b3d601
ycnham <ycnham@gmail.com>
2026-02-07 11:27:17 +0900

    first commit

---
+++ src/main/java/com/example/ticketing_system_project/TicketingSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketing_system_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemProjectApplication.class, args);
+	}
+
+}
